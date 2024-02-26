0f58e8d87a9b39b336c7a4b2a39c38dd371e56fb
KeluhingBavui <daniel01.jonathan@gmail.com>
2024-02-27 02:12:51 +0800

    Change Contact button to shimmer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -347,9 +347,9 @@ export default function Home() {
                 </p>
                 <div className="flex gap-4">
                     <Link href="mailto:daniel01.jonathan@gmail.com" target="_blank">
-                        <Button variant="default" size="lg">
+                        <button className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
                             Say Hi!
-                        </Button>
+                        </button>
                     </Link>
                 </div>
             </section>
